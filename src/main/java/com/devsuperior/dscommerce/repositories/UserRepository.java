4fb2b0225c5f0da6b3e0cdf480a1abfe97b5ef17
Luan M Paes <luanmp25@gmail.com>
2025-10-28 17:46:45 -0300

    ProductMinDTO

--- src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/dscommerce/repositories/UserRepository.java
@@ -1,10 +1,12 @@
 package com.devsuperior.dscommerce.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.projections.UserDetailsProjection;
 
@@ -19,4 +21,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
         WHERE tb_user.email = :email
     """)
     List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+    
+    Optional<User> findByEmail(String email);
 }
